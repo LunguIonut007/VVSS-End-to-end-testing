39982e4c74bb92dc25cd263c1946a232f170e0b9
Emanuel <emanuelhuluta@yahoo.com>
2020-05-16 23:06:10 +0300

    create announcement

--- src/test/java/com/lih/features/test/AnnouncementTest.java
+++ src/test/java/com/lih/features/test/AnnouncementTest.java
@@ -8,12 +8,13 @@ import net.thucydides.core.annotations.Steps;
 import net.thucydides.junit.annotations.UseTestDataFrom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SerenityParameterizedRunner.class)
-@UseTestDataFrom("src/test/resources/ValidLoginTestData.csv")
+@UseTestDataFrom("src/test/resources/AnnouncementTestData.csv")
 public class AnnouncementTest {
     @Managed(uniqueSession = true)
     public WebDriver webdriver;
@@ -23,6 +24,8 @@ public class AnnouncementTest {
 
     public String email;
     public String password;
+    public String title;
+    public String description;
 
     @Issue("Announcement #1 - navigate")
     @Test
@@ -41,7 +44,9 @@ public class AnnouncementTest {
         user.login(email, password);
         Thread.sleep(1000);
         user.goToAnnouncements(webdriver);
-        assertEquals("is over", "is not");
+       // assertEquals("is over", "is not");
+        Thread.sleep(1000);
+        user.createAnnouncement(title,description);
     }
 
     @Issue("Announcement #3 - delete")
--- src/test/java/com/lih/steps/serenity/EndUserSteps.java
+++ src/test/java/com/lih/steps/serenity/EndUserSteps.java
@@ -1,5 +1,6 @@
 package com.lih.steps.serenity;
 
+import com.lih.pages.AnnouncementPage;
 import com.lih.pages.HomePage;
 import com.lih.pages.LoginPage;
 import net.thucydides.core.annotations.Step;
@@ -9,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 public class EndUserSteps {
     public LoginPage loginPage;
     public HomePage homePage;
-
+    public AnnouncementPage announcementPage;
     @Step
     public void openPage() {
         loginPage.open();
@@ -29,5 +30,10 @@ public class EndUserSteps {
     @Step
     public void goToAnnouncements(WebDriver webDriver) {
         homePage.goToAnnouncements(webDriver);
+    }
+
+    @Step
+    public void createAnnouncement(String title,String description){
+        announcementPage.createAnnouncement(title,description);
     }
 }
